6b3f04662ff730b91306c1b32da729270c377686
phanvanhobryan-dev <phanvanhobryan@gmail.com>
2026-05-09 17:21:39 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -466,8 +466,8 @@ function PageProfil({ isMobile, isTablet, navigate }) {
             {/* Infos contact */}
             <div style={{display:"flex", flexDirection:"column", gap:12}}>
               {[
-                {icon:"◉", label:"Localisation", value:"Paris, France"},
-                {icon:"◆", label:"Email", value:"phanvanhobryan@gmail.com" href:"phanvanhobryan@gmail.com"},
+                {icon:"◉", label:"Numéros", value:"0614281909, France"},
+                {icon:"◆", label:"Email", value:"phanvanhobryan@gmail.com", href:"mailto:phanvanhobryan@gmail.com"},
                 {icon:"⬢", label:"LinkedIn", value:"bryanphanvanho", href:"https://www.linkedin.com/in/bryan-phan-van-ho-6bbb931b3/"},
                 {icon:"★", label:"Instagram",  value:"tenshi.blossom",        href:"https://www.instagram.com/tenshi.blossom/"},
               ].map((item) => (
